eef88c644fc37f7a2636b529b7d9526c7a58f12a
Joe Keohah <jkeohan@gmail.com>
2016-03-08 17:34:00 -0500

    first commit

--- thinkful-angular/1.5.5/js/app.js
+++ thinkful-angular/1.5.5/js/app.js
@@ -4,40 +4,47 @@ var myApp = angular.module("myApp", ['ngAnimate']);
 myApp.controller('flickrCtrl', ['$scope','$http',function($scope,$http){ 
 
 	'use strict';
+	//variables used to define which msg to display
 	$scope.searchMsg = false;
 	$scope.resultMsg = false;
 	$scope.errorMsg = false;
 
-		$scope.submit = function() {
+	$scope.notificationMsg = ""
+	//executed when submit button is clicked
+	$scope.submit = function() {
 		if($scope.searchForm.$invalid) {
+			$scope.notificationMsg = "Please enter a search term...."
 			console.log($scope.searchForm.$invalid);
 		}
 		else {
+			$scope.notificationMsg = "Searching Flickr for photos tagged with " + $scope.searchQuery
 			$scope.searchQueryCopy = $scope.searchQuery;
-			// $scope.searchQuery = ' ';
-			$scope.resultMsg = false;
-			$scope.searchMsg = true;
-			//CALL FETCHIMAGE 
+			// $scope.resultMsg = false;
+			// $scope.searchMsg = true;
+			//call fetchImage func with query.  
 			fetchImage($scope.searchQueryCopy);
-		}
-			
+		}			
 	};
-
+	//fetchImage will remove resultsMsg and exeute flickr func which makes the API call
 	function fetchImage(searchQuery){
-		$scope.resultsMsg = false;
+		// $scope.resultsMsg = false;
 		flickr(searchQuery)//$HTTP IS RETURNED SO A PROMISE CAN BE LEVEREAGED
+		//.then used instead of deprecated .success\.error func
+		//if successful execute updateView otherwise errorView
 			.then(updateView, errorView);
-		
+
 		function updateView(result) {
 			console.log(result);
-			$scope.searchMsg = false;
-			$scope.resultsMsg = true;
+			// $scope.searchMsg = false;
+			// $scope.resultsMsg = true;
+
 	      	$scope.result = result.data.photos.photo;
 	      	$scope.results = $scope.result.length;
+	      	$scope.notificationMsg = "We found " + $scope.results +  " search result(s) for " + $scope.searchQuery
 		}
 
 		function errorView(){
-			$scope.searchMsg = false;
+			$scope.notificationMsg = "There was an issue running the query"
 		}
 	}
 
